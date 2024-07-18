a7966df0b8340e34d931fc4b9d5dded2eeae088a
Beckham <guilhermebeckham14@hotmail.com>
2024-07-17 21:39:54 -0300

    funcao main temporária para testes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"importador_Excel/internal/scripts"
+	"sync"
+)
+
+func main() {
+	excelFileName := "./arquivo_teste.xlsx"
+
+	ch := make(chan []string) // Canal com buffer
+
+	var wg sync.WaitGroup
+
+	// Número de consumidores
+	numConsumers := 4
+
+	// Adiciona o número de consumidores ao grupo de espera
+	wg.Add(numConsumers)
+
+	// Inicia as goroutines consumidoras
+	for i := 0; i < numConsumers; i++ {
+		go scripts.ConsumeFileAndSaveData(ch, &wg)
+	}
+
+	// Lê o arquivo Excel e envia os dados para o canal
+	scripts.ReadFile(excelFileName, ch)
+
+	// Espera todas as goroutines consumidoras terminarem
+	wg.Wait()
+
+}
