483e44c3132a5e47998e5ecb707e179ab26dd6ba
Beckham <guilhermebeckham14@hotmail.com>
2024-07-19 20:16:09 -0300

    feat: adicionando novas rotas

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -2,12 +2,21 @@ package router
 
 import (
 	"importador_Excel/internal/controllers"
+	"importador_Excel/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(r *gin.Engine) {
 
-	r.POST("/saveData", controllers.UploadFileAndSaveData)
+	//Rotas disponiveis da API
+	r.POST("/createUser", controllers.CreateUser)
+	r.POST("/login", controllers.Login)
+
+	app := r.Group("/app")
+	app.Use(middleware.AuthRequired())
+	{
+		app.POST("/saveData", controllers.UploadFileAndSaveData)
+	}
 
 }
